bb8d3b7e776334d8fc296559e5d8ed223c90963b
Zubair Hassan <zubairhassan652@gmail.com>
2026-07-05 12:07:13 +0500

    flower is added

--- celery_app.py
+++ celery_app.py
@@ -1,12 +1,14 @@
 from celery import Celery
 import os
+import sys
 
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
 celery_app = Celery(
     "myapp",
     broker=REDIS_URL,
-    backend=REDIS_URL
+    backend=REDIS_URL,
+    include=["tasks"]
 )
 
 celery_app.conf.update(
@@ -16,3 +18,8 @@ celery_app.conf.update(
     timezone="UTC",
     enable_utc=True,
 )
+
+if sys.platform == "win32":
+    celery_app.conf.update(
+        worker_pool="solo",        # ← use solo pool on Windows
+    )
